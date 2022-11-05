2c4e21f35bcd16f5d4d16568261f2b617aabe4b4
vananh2110211 <80112389+vananh2110211@users.noreply.github.com>
2022-11-05 23:38:12 +0700

    Lab24h

---
+++ Debug/Core/Src/subdir.mk
@@ -0,0 +1,57 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/button_reading.c \
+../Core/Src/fsm_midterm.c \
+../Core/Src/globalVar.c \
+../Core/Src/led_7_seg.c \
+../Core/Src/main.c \
+../Core/Src/software_timer.c \
+../Core/Src/stm32f1xx_hal_msp.c \
+../Core/Src/stm32f1xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f1xx.c 
+
+OBJS += \
+./Core/Src/button_reading.o \
+./Core/Src/fsm_midterm.o \
+./Core/Src/globalVar.o \
+./Core/Src/led_7_seg.o \
+./Core/Src/main.o \
+./Core/Src/software_timer.o \
+./Core/Src/stm32f1xx_hal_msp.o \
+./Core/Src/stm32f1xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f1xx.o 
+
+C_DEPS += \
+./Core/Src/button_reading.d \
+./Core/Src/fsm_midterm.d \
+./Core/Src/globalVar.d \
+./Core/Src/led_7_seg.d \
+./Core/Src/main.d \
+./Core/Src/software_timer.d \
+./Core/Src/stm32f1xx_hal_msp.d \
+./Core/Src/stm32f1xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f1xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103x6 -c -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/button_reading.d ./Core/Src/button_reading.o ./Core/Src/button_reading.su ./Core/Src/fsm_midterm.d ./Core/Src/fsm_midterm.o ./Core/Src/fsm_midterm.su ./Core/Src/globalVar.d ./Core/Src/globalVar.o ./Core/Src/globalVar.su ./Core/Src/led_7_seg.d ./Core/Src/led_7_seg.o ./Core/Src/led_7_seg.su ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/software_timer.d ./Core/Src/software_timer.o ./Core/Src/software_timer.su ./Core/Src/stm32f1xx_hal_msp.d ./Core/Src/stm32f1xx_hal_msp.o ./Core/Src/stm32f1xx_hal_msp.su ./Core/Src/stm32f1xx_it.d ./Core/Src/stm32f1xx_it.o ./Core/Src/stm32f1xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f1xx.d ./Core/Src/system_stm32f1xx.o ./Core/Src/system_stm32f1xx.su
+
+.PHONY: clean-Core-2f-Src
+
